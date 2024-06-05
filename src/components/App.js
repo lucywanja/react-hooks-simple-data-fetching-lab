074322f1916447b13aa9f8d6b67e819fb684ee6a
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-06-05 12:10:04 +0300

    Created the App componentto display dog images

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogUrl, setDogUrl] = useState(null); //null indicates that there is no image
+   
+//   useEffect to fetch the dog image after the component mounts 
+  useEffect(() => {
+        const fetchDogImage = async () => {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        setDogUrl(data.message);
+        };
+
+    fetchDogImage();
+  }, []); //Empty dependancy so that we run our fetch only once
+
+  return (
+    <div className="App">
+      {dogUrl ? (
+        <img src={dogUrl} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
